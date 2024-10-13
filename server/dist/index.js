6b1477688ee9126f59fb0b991031e471b4415483
Wahyu Rifia Rizki <wahyurifia927@gmail.com>
2024-10-13 21:45:23 +0700

    okelah

--- server/dist/index.js
+++ server/dist/index.js
@@ -7,11 +7,14 @@ const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const user_controller_1 = require("./user/user.controller");
+const serve_favicon_1 = __importDefault(require("serve-favicon"));
+const path_1 = __importDefault(require("path"));
 dotenv_1.default.config();
 const PORT = process.env.PORT || 2000;
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
+app.use((0, serve_favicon_1.default)(path_1.default.join(__dirname, "public", "favicon.ico")));
 app.use("/users", user_controller_1.router);
 app.listen(PORT, () => {
     console.log("server is running on port " + PORT);
--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,8 @@ import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { router } from "./user/user.controller";
+import favicon from "serve-favicon";
+import path from "path";
 
 dotenv.config();
 const PORT = process.env.PORT || 2000;
@@ -9,6 +11,7 @@ const PORT = process.env.PORT || 2000;
 const app: Application = express();
 app.use(cors());
 app.use(express.json());
+app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
 
 app.use("/users", router);
 
