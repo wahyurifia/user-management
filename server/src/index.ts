9c98b57a44f2d18556e15a4eaa041fe31f2d0d06
Wahyu Rifia Rizki <wahyurifia927@gmail.com>
2024-10-13 18:32:44 +0700

    ok clear

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,9 +2,14 @@ import express, { Application } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { router } from "./user/user.controller";
+import { createClient } from "@supabase/supabase-js";
 
 dotenv.config();
 const PORT = process.env.PORT;
+
+const supabaseUrl: string = process.env.SUPABASE_URL!;
+const supabaseKey: string = process.env.SUPABASE_KEY!;
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 const app: Application = express();
 app.use(cors());
