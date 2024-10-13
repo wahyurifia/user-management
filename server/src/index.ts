e2491a75946d5a4c08e4eac46dcccddf972a858c
Wahyu Rifia Rizki <wahyurifia927@gmail.com>
2024-10-13 21:13:35 +0700

    abis ini sebat

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,8 +2,6 @@ import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { router } from "./user/user.controller";
-import favicon from "serve-favicon";
-import path from "path";
 
 dotenv.config();
 const PORT = process.env.PORT || 2000;
@@ -13,7 +11,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/users", router);
-app.use(favicon(path.join(__dirname, "../public", "favicon.ico")));
 
 app.listen(PORT, () => {
   console.log("server is running on port " + PORT);
