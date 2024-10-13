d425638389a20fe525e3415acfc8e4d41f6208da
Wahyu Rifia Rizki <wahyurifia927@gmail.com>
2024-10-13 20:53:36 +0700

    coba lagi laH

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,9 @@
-import express, { Application } from "express";
+import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { router } from "./user/user.controller";
+import favicon from "serve-favicon";
+import path from "path";
 
 dotenv.config();
 const PORT = process.env.PORT || 2000;
@@ -11,6 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/users", router);
+app.use(favicon(path.join(__dirname, "../public", "favicon.ico")));
 
 app.listen(PORT, () => {
   console.log("server is running on port " + PORT);
