6b50032821453484815e340a086fa11544685388
Wahyu Rifia Rizki <wahyurifia927@gmail.com>
2024-10-31 18:13:08 +0700

    menambahkan loading

--- client/src/components/UserList.tsx
+++ client/src/components/UserList.tsx
@@ -19,6 +19,7 @@ const UserList: React.FC = () => {
   const [users, setUser] = useState<User[]>([]);
   const [selectedUser, setSelectedUser] = useState<User | null>(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   const handleUserClick = (user: User) => {
     setSelectedUser(user);
@@ -34,12 +35,17 @@ const UserList: React.FC = () => {
     getUsers();
   }, []);
   const getUsers = async () => {
-    const response = await axios.get(
-      `https://server-api-lilac.vercel.app/users`
-    );
-    console.log(response.data.users);
-
-    setUser(response.data.users);
+    try {
+      setLoading(true);
+      const response = await axios.get(
+        `https://server-api-lilac.vercel.app/users`
+      );
+      setUser(response.data.users);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
   };
 
   const deleteUser = async (id: number) => {
@@ -63,69 +69,94 @@ const UserList: React.FC = () => {
             Add User
           </Link>
         </div>
-        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-            <tr>
-              <th scope="col" className="px-6 py-3">
-                No
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Name
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Email
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Gender
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Actions
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {users.map((user, index) => (
-              <tr
-                key={user?.id}
-                className={`bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600 ${
-                  index === users.length - 1 &&
-                  "border-none dark:border-gray-700"
-                }`}
+
+        {loading ? (
+          <div className="w-full flex items-center justify-center h-40 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
+            <div role="status">
+              <svg
+                aria-hidden="true"
+                className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                viewBox="0 0 100 101"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
               >
-                <td className="px-6 py-4">{index + 1}</td>
-                <th
-                  scope="row"
-                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                >
-                  {user?.name!}
+                <path
+                  d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                  fill="currentColor"
+                />
+                <path
+                  d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                  fill="currentFill"
+                />
+              </svg>
+              <span className="sr-only">Loading...</span>
+            </div>
+          </div>
+        ) : (
+          <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+              <tr>
+                <th scope="col" className="px-6 py-3">
+                  No
                 </th>
-                <td className="px-6 py-4">{user?.email!}</td>
-                <td className="px-6 py-4">{user?.gender}</td>
-                <td className="px-6 py-4">
-                  <Link
-                    to={`/edit/${user?.id}`}
-                    className="pr-6 font-medium  dark:text-blue-500 hover:underline"
-                  >
-                    Update
-                  </Link>
-                  <button
-                    className="font-medium  dark:text-red-500 hover:underline"
-                    onClick={() => handleUserClick(user)}
-                    type="button"
-                  >
-                    Delete
-                  </button>
-                </td>
+                <th scope="col" className="px-6 py-3">
+                  Name
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Email
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Gender
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Actions
+                </th>
               </tr>
-            ))}
-            <Modal
-              isOpen={isModalOpen}
-              onClose={closeModal}
-              user={selectedUser}
-              onConfirm={deleteUser}
-            />
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {users.map((user, index) => (
+                <tr
+                  key={user?.id}
+                  className={`bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600 ${
+                    index === users.length - 1 &&
+                    "border-none dark:border-gray-700"
+                  }`}
+                >
+                  <td className="px-6 py-4">{index + 1}</td>
+                  <th
+                    scope="row"
+                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                  >
+                    {user?.name!}
+                  </th>
+                  <td className="px-6 py-4">{user?.email!}</td>
+                  <td className="px-6 py-4">{user?.gender}</td>
+                  <td className="px-6 py-4">
+                    <Link
+                      to={`/edit/${user?.id}`}
+                      className="pr-6 font-medium  dark:text-blue-500 hover:underline"
+                    >
+                      Update
+                    </Link>
+                    <button
+                      className="font-medium  dark:text-red-500 hover:underline"
+                      onClick={() => handleUserClick(user)}
+                      type="button"
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
+              <Modal
+                isOpen={isModalOpen}
+                onClose={closeModal}
+                user={selectedUser}
+                onConfirm={deleteUser}
+              />
+            </tbody>
+          </table>
+        )}
       </div>
     </div>
   );
